cb51e05445e52f406b101178b080cb63d223656c
aditya-shah-001 <adityashah.001.as@gmail.com>
2021-06-14 13:25:01 +0530

    created day1 folder

---
+++ DAY-1/basic.py
@@ -0,0 +1,91 @@
+'''
+python functional as well as object oriented prog lang
+python are totaly based on objects and classes
+
+
+# var name has to be start with alpha numeric
+# it should not start  with spcl char and keyword
+# cannot start with numbers
+# var can stat with _(underscore)
+
+true = "Harry"
+list = 15 # wrong
+my_var = "Hello"
+_myvar = "Hello"
+my_var = "Hello"
+
+5myvar = "hello" #bcz start int
+my-var = "harry" #- is not expected
+my var =  "hie"  # spaces are not allowed
+
+age = 15
+AGE = 15
+'''
+
+
+# num = 12 #int
+# str1 = "hello"
+# str2 = 'hello'
+# str3 = ''' hello my name is harry '''
+
+# print(type(str1))
+
+# num = int(input("Enter the number 1 :"))
+# print("User entered the number ", num)
+
+x,y,z = "apple","mango","orange"
+x, y, z = 'apple', 'orange', 'mango'
+# print(x, y, z, sep='\n')
+
+# print(x)
+# print(y)
+# print(z)
+
+
+'''
+string formatting ?
+
+fname = "Sandeep"
+age = 21
+city = "mumbai"
+hobby = "cricket"
+
+print("My first name is ", fname, "my age is ", age) #method 1
+
+print("My name is {}. my age is {}".format("Sandeep",21)) #string formatting  method 2
+
+print(f"My name is {fname}. my age is {age} i stay in {city} love to play {hobby}")  #method 3
+'''
+# num = int(input("Enter the number : "))
+# name = input("Enter your name : ")
+
+x = str(3)
+x = int(3)  # x = 3 
+name = "john"
+name = 'john'
+name = '''john'''
+
+print(type(x))
+
+
+'''
+My Poem is :
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+'''
+
+#print("My Poem is :", "Twinkle, twinkle, little star", "How I wonder what you are", "Up above the world so high", "Like a diamond in the sky", "Twinkle, twinkle, little star", "How I wonder what you are", sep="\n")
+
+print('''
+My Poem is :
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+''')
